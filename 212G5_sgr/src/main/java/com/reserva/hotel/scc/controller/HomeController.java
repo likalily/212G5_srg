2514ff383de0a61c1ac7a53047f0b4a0471fb677
Eliane Leite Andrade <elianeleiteandrade@yahoo.com.br>
2021-10-06 16:57:25 -0300

    Terceiro Commit

--- 212G5_sgr/src/main/java/com/reserva/hotel/scc/controller/HomeController.java
+++ 212G5_sgr/src/main/java/com/reserva/hotel/scc/controller/HomeController.java
@@ -3,23 +3,14 @@ package com.reserva.hotel.scc.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.servlet.ModelAndView;
-
 @Controller
 public class HomeController {
-@GetMapping("/")
-public ModelAndView menu() {
-	return new ModelAndView("paginaMenu");
-}
 @GetMapping("/login")
 public ModelAndView autenticacao() {
-	return new ModelAndView("paginaLogin");
+return new ModelAndView("paginaLogin");
 }
-@GetMapping("/clientes")
-public ModelAndView cadastrarCliente() {
-	return new ModelAndView("cadastrarCliente");
-}
-@GetMapping("/fornecedores")
-public ModelAndView cadastrarFornecedor() {
-	return new ModelAndView("cadastrarFornecedor");
+@GetMapping("/")
+public ModelAndView home() {
+return new ModelAndView ("paginaMenu");
 }
 }
